8e0ac5631f16bb49df67900118e8c164a7fe6693
Abhay Manchanda <manchandaabhay7091@gmail.com>
2023-02-17 23:26:20 +0530

    counter changes

---
+++ app.js
@@ -0,0 +1,7 @@
+var increase = document.querySelector(".btn increase");
+var decrease = document.querySelector(".btn decrease");
+var reset = document.querySelector(".btn reset");
+
+increase.addEventListener(("onClick") => {
+    
+})
